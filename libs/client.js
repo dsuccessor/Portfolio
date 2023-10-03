d36d13088a08a778595f330addc3096de86a77c8
Salaudeen Kehinde <salaudeenkr@gmail.com>
2023-10-03 22:23:06 +0100

    Switching out of localhost

--- libs/client.js
+++ libs/client.js
@@ -4,14 +4,14 @@ const {
   InMemoryCache,
 } = require("@apollo/client");
 
-// const client = new ApolloClient({
-//   uri: "https://classicportfolio-api.vercel.app/portfolio",
-//   cache: new InMemoryCache(),
-// });
-
 const client = new ApolloClient({
-  uri: "http://localhost:4000/portfolio",
+  uri: "https://classicportfolio-api.vercel.app/portfolio",
   cache: new InMemoryCache(),
 });
+
+// const client = new ApolloClient({
+//   uri: "http://localhost:4000/portfolio",
+//   cache: new InMemoryCache(),
+// });
 
 export { client, ApolloProvider };
